ee4dca3a93026dfa253b5500f4b6ae6e2d83b1f1
Ruki <rukitest13@gmail.com>
2020-09-07 14:38:50 +0530

    Added new food categories
    Added new food categories

--- scripts/configs.js
+++ scripts/configs.js
@@ -2,14 +2,15 @@
  * Script to hold configuration values
  */
 
-export let category_1_values = ['American', 'Asian', 'British', 'Dutch', 'European', 'Fast Food', 'French', 'Fusion', 'German', 'Greek', 'Home Baker', 'Home Based', 'South Indian', 'North Indian', 'Iranian', 'Italian', 'Jaffna', 'Japanese', 'Korean', 'Mainland Chinese', 'Mediterranean', 'Mexican', 'Mongolian', 'Pakistani', 'Swiss', 'South-East Asian', 'Spanish', 'Sri Lankan', 'Sri Lankan Chinese', 'Vietnamese', 'Syrian', 'Thai', 'Western']
-export let category_2_values = ['Rice', 'Burger', 'Pizza', 'Pasta', 'BBQ', 'Fruit Juice', 'Biriyani', 'Bites', 'Cakes', 'Coffee', 'Dosa', 'Desserts', 'Bubble Tea', 'Street Food', 'Roti', 'Soup', 'Noodles', 'Short eats', 'Indian Sweets', 'Bread', 'Sandwiches', 'Shawarma', 'Sushi', 'Fast Food/Franchise', 'Ice Cream', 'Yoghurt', 'Shakes', 'Waffles', 'Shellfish', 'Seafood']
-export let category_3_values = ['Chicken', 'Beef', 'Mutton', 'Pork', 'Lamb', 'Quail', 'Eggs', 'Vegan', 'Vegetarian', 'Crab', 'Healthy', 'Turkey', 'Duck', 'Cuttlefish', 'Prawns', 'Gluten-free', 'Lactose Intolerant', 'Milk', 'Nuts']
-export let category_4_values = ['Spicy', 'Cheesy', 'Sweet', 'Pickles']
-export let category_5_values = ['Breakfast', 'Lunch', 'Dinner', 'Brunch', 'Snacks']
+export let category_1_values = ['American', 'Asian', 'British', 'Dutch', 'European', 'Fast Food', 'French', 'Fusion', 'German', 'Greek', 'Home Baker', 'Home Based', 'Indonesian', 'Iranian', 'Italian', 'Jaffna', 'Japanese', 'Korean', 'Mainland Chinese', 'Mediterranean', 'Mexican', 'Mongolian', 'North Indian', 'Pakistani', 'South Indian', 'South-East Asian', 'Spanish', 'Sri Lankan', 'Sri Lankan Chinese', 'Swiss', 'Syrian', 'Thai', 'Vietnamese', 'Western']
+export let category_2_values = ['BBQ', 'Biriyani', 'Bites', 'Bread', 'Bubble Tea', 'Burger', 'Cakes', 'Coffee', 'Condiments', 'Curry', 'Desserts', 'Dosa', 'Fast Food', 'Fruit Juice', 'Ice Cream', 'Indian Sweets', 'Kottu', 'Noodles', 'Pasta', 'Pizza', 'Rice', 'Roti', 'Salad', 'Sandwiches', 'Seafood', 'Shakes', 'Shawarma', 'Shellfish', 'Short eats', 'Soft Drinks', 'Soup', 'Street Food', 'Submarine', 'Sushi', 'Tea', 'Waffles', 'Wrap']
+export let category_3_values = ['Beef', 'Chicken', 'Crab', 'Cuttlefish', 'Dairy', 'Duck', 'Eggs', 'Gluten-free', 'Healthy', 'Kid-friendly', 'Lactose-free', 'Lamb', 'Mutton', 'Nuts', 'Pescetarian', 'Pork', 'Prawns', 'Quail', 'Sausages', 'Sugar-free', 'Turkey', 'Vegan', 'Vegetarian', 'Water']
+export let category_4_values = ['Breakfast', 'Brunch', 'Dinner', 'Lunch', 'Snacks']
+export let category_5_values = ['Cold', 'Frozen', 'Hot']
+export let category_6_values = ['Cheesy', 'Crispy', 'Devilled', 'Fried', 'Garlic', 'Grilled', 'Masala', 'Pickles', 'Roast', 'Spicy', 'Stew', 'Sweet', 'Tandoori']
 export let hidden_column_indexes = [3]
-export let column_headers_user = ['ItemId', 'ItemName', 'RestaurantId', 'LastUpdatedDate', 'Category1', 'Category2', 'Category3', 'Category4', 'Category5']
-export let column_headers_admin = ['ItemId', 'ItemName', 'RestaurantId', 'LastUpdatedDate', 'Category1', 'Category2', 'Category3', 'Category4', 'Category5', 'Verified']
+export let column_headers_user = ['ItemId', 'ItemName', 'RestaurantId', 'LastUpdatedDate', 'Category1', 'Category2', 'Category3', 'Category4', 'Category5', 'Category6']
+export let column_headers_admin = ['ItemId', 'ItemName', 'RestaurantId', 'LastUpdatedDate', 'Category1', 'Category2', 'Category3', 'Category4', 'Category5', 'Category6', 'Verified']
 export let columns_user = [
     {
         editor: false
@@ -43,6 +44,11 @@ export let columns_user = [
     {
         type: 'dropdown',
         source: category_5_values,
+        trimDropdown: false
+    },
+    {
+        type: 'dropdown',
+        source: category_6_values,
         trimDropdown: false
     }
 ]
